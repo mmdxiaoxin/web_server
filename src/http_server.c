ee37fcb3080880462a2bb5ace1aa0861058db21e
xiaoxin <782446723@qq.com>
2023-05-30 16:37:02 +0800

    使用多线程的版本

--- src/http_server.c
+++ src/http_server.c
@@ -13,8 +13,23 @@
 #include "http_server.h"
 #include "http_request.h"
 
+const char *_root_dir;
+const char *_index;
+
+void *handle_client(void *arg)
+{
+    int client_sock = *(int *)arg;
+    handle_request(client_sock, _root_dir, _index);
+    close(client_sock);
+    free(arg);
+    return NULL;
+}
+
 void start_web_server(const char *root_directory, const char *root_index, int port)
 {
+    _root_dir = root_directory;
+    _index = root_index;
+
     int listen_sock;
     struct sockaddr_in server_addr;
 
@@ -48,7 +63,7 @@ void start_web_server(const char *root_directory, const char *root_index, int po
     }
 
     printf("Server is running on port %d\n", port);
-    
+
     while (1)
     {
         struct sockaddr_in client_addr;
@@ -64,36 +79,52 @@ void start_web_server(const char *root_directory, const char *root_index, int po
 
         printf("Accepted connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-        pid_t pid = fork();
-        if (pid == 0)
-        {
-            // 创建新会话
-            if (setsid() < 0)
-            {
-                perror("Failed to create new session");
-                exit(1);
-            }
+        // 多进程处理请求
+        // pid_t pid = fork();
+        // if (pid == 0)
+        // {
+        //     // 创建新会话
+        //     if (setsid() < 0)
+        //     {
+        //         perror("Failed to create new session");
+        //         exit(1);
+        //     }
 
-            close(listen_sock);
+        //     close(listen_sock);
 
-            // 关闭标准输入、输出和错误输出文件描述符
-            close(STDIN_FILENO);
-            close(STDOUT_FILENO);
-            close(STDERR_FILENO);
+        //     // 关闭标准输入、输出和错误输出文件描述符
+        //     close(STDIN_FILENO);
+        //     close(STDOUT_FILENO);
+        //     close(STDERR_FILENO);
 
-            handle_request(client_sock, root_directory, root_index);
-            close(client_sock);
-            exit(0);
-        }
-        else if (pid > 0)
+        //     handle_request(client_sock, root_directory, root_index);
+        //     close(client_sock);
+        //     exit(0);
+        // }
+        // else if (pid > 0)
+        // {
+        //     close(client_sock);
+        // }
+        // else
+        // {
+        //     perror("Failed to create child process");
+        //     close(client_sock);
+        // }
+
+        // 多线程处理请求
+        int *client_sock_ptr = (int *)malloc(sizeof(int));
+        *client_sock_ptr = client_sock;
+
+        pthread_t thread;
+        if (pthread_create(&thread, NULL, handle_client, (void *)client_sock_ptr) != 0)
         {
+            perror("Failed to create thread");
             close(client_sock);
+            free(client_sock_ptr);
+            continue;
         }
-        else
-        {
-            perror("Failed to create child process");
-            close(client_sock);
-        }
+
+        pthread_detach(thread);
     }
 
     close(listen_sock);
